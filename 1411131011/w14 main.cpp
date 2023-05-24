d86c1c6c2459a70648fdcae695203401e0958135
1411131011 <113488161+kyw31@users.noreply.github.com>
2023-05-24 11:05:03 +0800

    Update and rename w14 Date.cpp(main) to w14 main.cpp

---
+++ 1411131011/w14 main.cpp
@@ -0,0 +1,44 @@
+// Fig. 9.3: fig09_03.cpp
+// Program to test class Time.
+// NOTE: This file must be compiled with Time.cpp.
+#include <iostream>
+#include <stdexcept> // for invalid_argument exception class
+#include "Date.h" // include definitionof class Time from Time.h
+using namespace std;
+
+int main()
+{
+    Date t; // instantiate object t of class Time
+
+    // output Time object t's initial values
+    cout << "The initial universal Date is ";
+    t.printUniversal(); // 00:00:00
+    cout << "\nThe initial ROC Date is ";
+    t.printROC(); // 12:00:00 AM
+
+    t.setDate(1990, 1, 1); // change time
+
+    // output Time object t's new values
+    cout << "\n\nUniversal time after setDate is ";
+    t.printUniversal(); // 13:27:06
+    cout << "\nROC time after setDate is ";
+    t.printROC(); // 1:27:06 PM
+
+    // attempt to set the time with invalid values
+    try
+    {
+        t.setDate(2017, 03, 32); // all values out of range
+    } // end try
+    catch (invalid_argument& e)
+    {
+        cout << "\n\nException: " << e.what() << endl;
+    } // end catch
+
+    // output t's values after specifying invalid values
+    cout << "\nAfter attempting invalid settings:"
+        << "\nUniversal Date: ";
+    t.printUniversal(); // 13:27:06
+    cout << "\nROC Date: ";
+    t.printROC(); // 1:27:06 PM
+    cout << endl;
+} // end main
