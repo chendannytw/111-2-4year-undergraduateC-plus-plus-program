f56b5238ed65d3d5894c42072c780673386f7a5a
1411131045 <113488050+1411131045@users.noreply.github.com>
2023-05-31 10:24:42 +0800

    Update Date.h

--- 1411131045/Date.h
+++ 1411131045/Date.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef DATE_H
+#define DATE_H
+
+// Time class definition
+class Date
+{
+public:
+	Date(int, int, int); // constructor
+	void setDate(int, int, int); // set hour, minute and second
+	void printUniversal() const; // 印出西元年格式 format
+	void printStandard() const; // 印出民國年格式 format
+private:
+	unsigned int year; // 0 - 9999 
+	unsigned int month; // 1 - 12
+	unsigned int day; // 1 - 31
+}; // end class Time
+
+#endif
