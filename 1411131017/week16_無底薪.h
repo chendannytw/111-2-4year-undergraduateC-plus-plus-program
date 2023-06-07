fe0fef209cdd17b33b064b6df67010b2103b0f0b
1411131017 <113487838+1411131017@users.noreply.github.com>
2023-06-07 10:49:49 +0800

    Create week16_無底薪.h

---
+++ 1411131017/week16_無底薪.h
@@ -0,0 +1,39 @@
+// Fig. 11.4: CommissionEmployee.h
+// CommissionEmployee class definition represents a commission employee.
+#ifndef COMMISSION_H
+#define COMMISSION_H
+
+#include <string> // C++ standard string class
+
+class CommissionEmployee
+{
+public:
+    CommissionEmployee(const std::string&, const std::string&,
+        const std::string&, double = 0.0, double = 0.0);
+
+    void setFirstName(const std::string&); // set first name
+    std::string getFirstName() const; // return first name
+
+    void setLastName(const std::string&); // set last name
+    std::string getLastName() const; // return last name
+
+    void setSocialSecurityNumber(const std::string&); // set SSN
+    std::string getSocialSecurityNumber() const; // return SSN
+
+    void setGrossSales(double); // set gross sales amount
+    double getGrossSales() const; // return gross sales amount
+
+    void setCommissionRate(double); // set commission rate (percentage)
+    double getCommissionRate() const; // return commission rate
+
+    double earnings() const; // calculate earnings
+    void print() const; // print CommissionEmployee object
+//private:
+    std::string firstName;
+    std::string lastName;
+    std::string socialSecurityNumber;
+    double grossSales; // gross weekly sales
+    double commissionRate; // commission percentage
+}; // end class CommissionEmployee
+
+#endif
