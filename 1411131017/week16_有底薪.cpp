7213f369a89e99cb010dbf9c9650320e19fa0323
1411131017 <113487838+1411131017@users.noreply.github.com>
2023-06-07 10:48:21 +0800

    Create week16_有底薪.cpp

---
+++ 1411131017/week16_有底薪.cpp
@@ -0,0 +1,49 @@
+// Fig. 11.11: BasePlusCommissionEmployee.cpp
+// Class BasePlusCommissionEmployee member-function definitions.
+#include <iostream>
+#include <stdexcept>
+#include "BasePlusCommissionEmployee.h" // class definition
+using namespace std;
+
+// constructor
+BasePlusCommissionEmployee::BasePlusCommissionEmployee(
+    const string& first, const string& last, const string& ssn,
+    double sales, double rate, double salary)
+    // explicitly call base-class constructor
+    : CommissionEmployee(first, last, ssn, sales, rate)
+{
+    setBaseSalary(salary); // validate and store base salary
+} // end BasePlusCommissionEmployee constructor
+
+// set base salary
+void BasePlusCommissionEmployee::setBaseSalary(double salary)
+{
+    if (salary >= 0.0)
+        baseSalary = salary;
+    else
+        throw invalid_argument("Salary must be >= 0.0");
+} // end function setBaseSalary
+
+// return base salary
+double BasePlusCommissionEmployee::getBaseSalary() const
+{
+    return baseSalary;
+} // end function getBaseSalary
+
+// calculate earnings
+double BasePlusCommissionEmployee::earnings() const
+{
+    // derived class cannot access the base class's private data
+    return baseSalary + (commissionRate * grossSales);
+} // end function earnings
+
+// print BasePlusCommissionEmployee object
+void BasePlusCommissionEmployee::print() const
+{
+    // derived class cannot access the base class's private data
+    cout << "base-salaried commission employee: " << firstName << ' '
+        << lastName << "\nsocial security number: " << socialSecurityNumber
+        << "\ngross sales: " << grossSales
+        << "\ncommission rate: " << commissionRate
+        << "\nbase salary: " << baseSalary;
+} // end function print
