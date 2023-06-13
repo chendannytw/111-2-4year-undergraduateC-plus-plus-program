0021467e25106f1a7b408c445c047b953ac205c7
1411131047 <113487973+1411131047@users.noreply.github.com>
2023-06-13 23:16:17 +0800

    BasePlusCommissionEmployee(有底薪介面檔).h

--- 1411131047/(第二題)期末考程式碼(員工資料)/(第二題)期末考程式碼(員工資料)/BasePlusCommissionEmployee(有底薪介面檔).h
+++ 1411131047/(第二題)期末考程式碼(員工資料)/(第二題)期末考程式碼(員工資料)/BasePlusCommissionEmployee(有底薪介面檔).h
@@ -1,4 +1,4 @@
-// µ{¦¡³]­p´Á¥½¦Ò±M¥Î  ¦³©³Á~ªº­û¤uÃş§O¤¶­±ÀÉ  (³¯¤j¤¯¦Ñ®v­×§ï)
+// ç¨‹å¼è¨­è¨ˆæœŸæœ«è€ƒå°ˆç”¨  æœ‰åº•è–ªçš„å“¡å·¥é¡åˆ¥ä»‹é¢æª”  (é™³å¤§ä»è€å¸«ä¿®æ”¹)
 // BasePlusCommissionEmployee class derived from class 
 // CommissionEmployee.
 #ifndef BASEPLUS_H
@@ -25,17 +25,3 @@ private:
 #endif
 
 
-/**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- **************************************************************************/
